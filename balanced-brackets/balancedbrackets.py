e5b19f4ad4137a404a2773689ef25c2808fbea52
dmcdekker <denisemdekker@gmail.com>
2018-06-26 08:49:51 -0700

    complete balanced brackets

---
+++ balanced-brackets/balancedbrackets.py
@@ -0,0 +1,81 @@
+"""Does a given string have balanced pairs of brackets?
+
+Given a string, return True or False depending on whether the string
+contains balanced (), {}, [], and/or <>.
+
+Many of the same test cases from Balance Parens apply to the expanded
+problem, with the caveat that they must check all types of brackets.
+
+These are fine::
+
+   >>> has_balanced_brackets("<ok>")
+   True
+
+   >>> has_balanced_brackets("<{ok}>")
+   True
+
+   >>> has_balanced_brackets("<[{(yay)}]>")
+   True
+
+These are invalid, since they have too many open brackets::
+
+   >>> has_balanced_brackets("(Oops!){")
+   False
+
+   >>> has_balanced_brackets("{[[This has too many open square brackets.]}")
+   False
+
+These are invalid, as they close brackets that weren't open::
+
+   >>> has_balanced_brackets(">")
+   False
+
+   >>> has_balanced_brackets("(This has {too many} ) closers. )")
+   False
+
+Here's a case where the number of brackets opened matches
+the number closed, but in the wrong order::
+
+    >>> has_balanced_brackets("<{Not Ok>}")
+    False
+
+If you receive a string with no brackets, consider it balanced::
+
+   >>> has_balanced_brackets("No brackets here!")
+   True
+
+"""
+
+
+def has_balanced_brackets(phrase):
+    """Does a given string have balanced pairs of brackets?
+
+    Given a string as input, return True or False depending on whether the
+    string contains balanced (), {}, [], and/or <>.
+    """
+    # put all possible pairs in dict
+    pairs = {'}':'{', ']':'[', ')':'(', '>':'<'}
+    # list as stack to hold values
+    stack = []
+    # iterate through each char
+    for char in phrase:
+        # add to stack if open bracket (values)
+        if char in pairs.values():
+            stack.append(char)
+        # else if key value 
+        elif char in pairs:
+            # if empty stack or char doesn't equal what's on top
+          if not stack or pairs[char] != stack.pop():
+              return False
+    return not stack
+        
+
+        
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY BRACKETS!\n"
