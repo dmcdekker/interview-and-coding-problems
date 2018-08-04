63283778b78bdf4578ccce6dfa5f0af3fedf1d0f
dmcdekker <denisemdekker@gmail.com>
2018-08-04 14:39:46 -0700

    finish bst-valid

---
+++ tree-problems/bst-valid/bstvalid.py
@@ -0,0 +1,127 @@
+"""Is this binary search tree a valid BST?
+
+A valid binary search tree follows a specific rule. In our case,
+the rule is "left child must value must be less-than parent-value"
+and "right child must be greater-than-parent value".
+
+This rule is recursive, so *everything* left of a parent
+must less than that parent (even grandchildren or deeper)
+and everything right of a parent must be greater than.
+
+For example, this tree is valid::
+
+        4
+     2     6
+    1 3   5 7
+
+Let's create this tree and test that::
+
+    >>> t = Node(4,
+    ...       Node(2, Node(1), Node(3)),
+    ...       Node(6, Node(5), Node(7))
+    ... )
+
+    >>> t.is_valid()
+    True
+
+This tree isn't valid, as the left-hand 3 is wrong (it's less
+than 2)::
+
+        4
+     2     6
+    3 3   5 7
+
+Let's make sure that gets caught::
+
+    >>> t = Node(4,
+    ...       Node(2, Node(3), Node(3)),
+    ...       Node(6, Node(5), Node(7))
+    ... )
+
+    >>> t.is_valid()
+    False
+
+This tree is invalid, as the bottom-right 1 is wrong --- it is
+less than its parent, 6, but it's also less than its grandparent,
+4, and therefore should be left of 4::
+
+        4
+     2     6
+    1 3   1 7
+
+    >>> t = Node(4,
+    ...       Node(2, Node(1), Node(3)),
+    ...       Node(6, Node(1), Node(7))
+    ... )
+
+    >>> t.is_valid()
+    False
+"""
+
+
+class Node:
+    """Binary search tree node."""
+
+    def __init__(self, data, left=None, right=None):
+        """Create node, with data and optional left/right."""
+
+        self.left = left
+        self.right = right
+        self.data = data
+
+    def is_valid(self):
+        """Is this tree a valid BST?"""
+
+        def check_nodes(node, lt, gt):
+            """Check this node & recurse to children
+
+                lt: left children must be <= this
+                gt: right child must be >= this
+            """
+
+            # if not node
+            if node is None:
+                return True
+
+            # fail if parent > left node
+            if lt is not None and node.data > lt:
+                return False
+
+            # fail if parent < right node
+            if gt is not None and node.data < gt:
+                return False
+
+            # general case: check our left child
+            # all descendants of left child must be
+            # less than our data (and greater than
+            # whatever we had to be greater than).
+            if not check_nodes(node.left, node.data, gt):
+                return False
+
+            
+            # general case: check our right child
+            # all descendants of right child must be
+            # greater than our data (and less than
+            # whatever we had to be less than)
+            if not check_nodes(node.right, lt, node.data):
+                return False
+
+            # If we reach here, we're either a leaf node with
+            # valid data for lt/gt, or we're higher up, but
+            # our recursive calls downward succeeded. Either way,
+            # this is our winning base case.
+            return True
+
+        # Call our recursive function, starting here.
+        # Since we haven't yet gone left or right, we don't know
+        # our `lt` or `gt` values yet, so pass None for these.
+
+        return check_nodes(self, None, None)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED; THAT'S VALID!\n")
+
