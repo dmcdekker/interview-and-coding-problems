6e0ee6f89390037b05c214c4133bd7fb7c178c34
dmcdekker <denisemdekker@gmail.com>
2018-08-07 09:50:00 -0700

    finish check-balanced

---
+++ tree-problems/check-balanced/checkbalanced.py
@@ -0,0 +1,148 @@
+"""Is the tree at this node balanced?
+
+To make this a bit more readable, let's alias our class name:
+
+    >>> N = BinaryNode
+
+For a tree of 1 item:
+
+    >>> tree1 = N(1)
+    >>> tree1.is_balanced()
+    True
+
+For a tree of 2 items:
+
+  1
+ /
+2
+
+    >>> tree2 = N(1,
+    ...           N(2))
+    >>> tree2.is_balanced()
+    True
+
+Three:
+
+  1
+ / \
+2   3
+
+    >>> tree3 = N(1,
+    ...           N(2), N(3))
+    >>> tree3.is_balanced()
+    True
+
+Four:
+
+     1
+    / \
+   2   4
+  /
+ 3
+
+    >>> tree4 = N(1,
+    ...           N(2,
+    ...             N(3)),
+    ...           N(4))
+    >>> tree4.is_balanced()
+    True
+
+Five:
+
+     1
+   /---\
+  2     5
+ / \
+3   4
+
+    >>> tree5 = N(1,
+    ...           N(2,
+    ...             N(3), N(4)),
+    ...           N(5))
+    >>> tree5.is_balanced()
+    True
+
+Imbalanced Four:
+
+    1
+   /
+  2
+ / \
+3   4
+
+    >>> tree4i = N(1,
+    ...            N(2,
+    ...              N(3), N(4)))
+    >>> tree4i.is_balanced()
+    False
+
+Imbalanced Six:
+
+    1
+   / \
+  2   6
+ / \
+3   4
+   /
+  5
+
+    >>> tree6i = N(1,
+    ...         N(2,
+    ...       N(3), N(4,
+    ...           N(5))),
+    ...                   N(6))
+    >>> tree6i.is_balanced()
+    False
+"""
+
+
+class BinaryNode(object):
+    """Node in a binary tree."""
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def is_balanced(self):
+        """Is the tree at this node balanced?"""
+
+        # nested function declaration: only used by is_balanced
+
+        def num_descendants(node):
+            """Returns number of descendants or None if already imbalanced."""
+            # use recursion to add up nodes
+            # return of None means imbalanced
+
+            # base case
+            if not node:
+                return 0
+
+            
+            left = num_descendants(node.left)
+
+            # quick check for None: fail quickly
+            if left is None:
+                return None
+
+            right = num_descendants(node.right)
+            # quick check for None: fail quickly
+            if right is None:
+                return None
+
+            if abs(left - right) > 1:
+                # Heights vary by more than 1 -- imbalanced!
+                return None
+
+            # Height of this node is height of our deepest descendant + ourselves
+            return max(left, right) + 1
+
+        return num_descendants(self) is not None
+
+  
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TEST PASSED! GO GO GO!\n")
