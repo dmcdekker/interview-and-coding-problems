4dddd9c93983ea25d079e646552563192280a6fc
dmcdekker <denisemdekker@gmail.com>
2018-08-31 14:26:31 -0700

    finish day-5

---
+++ hackerrank-30-days/Day-5/loops.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = 2
+    for num in range(1, 11):
+        _sum = n * num
+        print ('{} x {} = {}'.format(n, num, _sum))
