16c1e4cbbc9a8b821b059e345179162d94f5ba60
dmcdekker <denisemdekker@gmail.com>
2018-09-18 10:31:59 -0700

    finish is bst

---
+++ interview-cake/trees-and-graphs/is_bst.py
@@ -0,0 +1,102 @@
+import unittest
+
+
+def is_binary_search_tree(root):
+    """
+    Given the root of a binary tree, return true if it is a binary search tree.
+    """
+
+    def is_bst_recursive(root, smallest, largest):
+
+        if root is None:
+            return True
+
+        # fail if parent is larger than left node
+        if largest is not None and root.value > largest:
+            return False
+        
+        # fail if parent < right node
+        if smallest is not None and root.value < smallest:
+            return False
+        
+        # all descendants of left child must be less than our data (and greater than
+        # whatever we had to be greater than).
+        if not is_bst_recursive(root.left, smallest, root.value):
+            return False
+    
+        # all descendants of right child must be less than our data (and greater than
+        # whatever we had to be greater than).
+        if not is_bst_recursive(root.right, root.value, largest):
+            return False
+        
+        # base case: successful traversal of tree
+        return True
+    
+    # recursive calls with none as largest/smallest values to initialize
+    return is_bst_recursive(root, None, None)
+    
+# Tests
+
+class Test(unittest.TestCase):
+
+    class BinaryTreeNode(object):
+
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
+
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
+
+    def test_valid_full_tree(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        right.insert_left(60)
+        right.insert_right(80)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+    def test_both_subtrees_valid(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(80)
+        left.insert_left(20)
+        left.insert_right(60)
+        right.insert_left(70)
+        right.insert_right(90)
+        result = is_binary_search_tree(tree)
+        self.assertFalse(result)
+
+    def test_descending_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(40)
+        left_left = left.insert_left(30)
+        left_left_left = left_left.insert_left(20)
+        left_left_left.insert_left(10)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+    def test_out_of_order_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        right = tree.insert_right(70)
+        right_right = right.insert_right(60)
+        right_right.insert_right(80)
+        result = is_binary_search_tree(tree)
+        self.assertFalse(result)
+
+    def test_one_node_tree(self):
+        tree = Test.BinaryTreeNode(50)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
