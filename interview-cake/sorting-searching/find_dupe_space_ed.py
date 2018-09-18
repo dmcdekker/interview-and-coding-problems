44ab1469a2d7f16e15bc0820ef2b44612d4edae2
dmcdekker <denisemdekker@gmail.com>
2018-09-18 10:16:34 -0700

    finish find dupe space edition

---
+++ interview-cake/sorting-searching/find_dupe_space_ed.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+def find_repeat(numbers):
+    '''Write a function which finds an integer that appears more than once in our list'''
+    numbers_seen = set()
+    for number in numbers:
+        if number in numbers_seen:
+            return number
+        else:
+            numbers_seen.add(number)
+
+    raise Exception('no duplicate!')
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_repeat([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
