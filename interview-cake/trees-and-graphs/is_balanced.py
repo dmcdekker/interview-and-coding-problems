5ec3f395acafd7aaf247907427b5f3f66005ef0c
dmcdekker <denisemdekker@gmail.com>
2018-09-18 10:25:58 -0700

    create trees and graphs and complete is_balanced

---
+++ interview-cake/trees-and-graphs/is_balanced.py
@@ -0,0 +1,108 @@
+import unittest
+
+# Note that I'm checking for balanced as opposed to superbalanced
+def is_balanced(root):
+    '''Write a function to see if a binary tree is balanced''' 
+        def get_height(root):
+            if root is None:
+                return 0
+            left, right = \
+                get_height(root.left), get_height(root.right)
+            if left < 0 or right < 0 or \
+               abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+        return (get_height(root) >= 0)
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    class BinaryTreeNode(object):
+
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
+
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
+
+    def test_full_tree(self):
+        tree = Test.BinaryTreeNode(5)
+        left = tree.insert_left(8)
+        right = tree.insert_right(6)
+        left.insert_left(1)
+        left.insert_right(2)
+        right.insert_left(3)
+        right.insert_right(4)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_both_leaves_at_the_same_depth(self):
+        tree = Test.BinaryTreeNode(3)
+        left = tree.insert_left(4)
+        right = tree.insert_right(2)
+        left.insert_left(1)
+        right.insert_right(9)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_leaf_heights_differ_by_one(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right.insert_right(7)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_leaf_heights_differ_by_two(self):
+        tree = Test.BinaryTreeNode(6)
+        left = tree.insert_left(1)
+        right = tree.insert_right(0)
+        right_right = right.insert_right(7)
+        right_right.insert_right(8)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_three_leaves_total(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right.insert_left(8)
+        right.insert_right(5)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    def test_both_subtrees_superbalanced(self):
+        tree = Test.BinaryTreeNode(1)
+        left = tree.insert_left(5)
+        right = tree.insert_right(9)
+        right_left = right.insert_left(8)
+        right.insert_right(5)
+        right_left.insert_left(7)
+        result = is_balanced(tree)
+        self.assertFalse(result)
+
+    def test_only_one_node(self):
+        tree = Test.BinaryTreeNode(1)
+        result = is_balanced(tree)
+        self.assertTrue(result)
+
+    # did not include this test
+    # def test_linked_list_tree(self):
+    #     tree = Test.BinaryTreeNode(1)
+    #     right = tree.insert_right(2)
+    #     right_right = right.insert_right(3)
+    #     right_right.insert_right(4)
+    #     result = is_balanced(tree)
+    #     self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
