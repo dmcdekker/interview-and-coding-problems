5432c84178c2db6331769b74d7eb8ce44405d85c
dmcdekker <denisemdekker@gmail.com>
2018-10-07 14:50:11 -0700

    finish longest substring

---
+++ easy-python-problems/longest-substring-no-repeats/longest_substring.py
@@ -0,0 +1,48 @@
+import unittest
+
+def length_of_longest_substring_no_repeats(s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # O(n^2)
+        # results = []
+        # for substring in range(len(string) + 1):
+        #     for index in range(len(string)+ 1 - substring):
+        #         word = string[index: index + substring]
+        #         if len(word) == len(set(word)):
+        #             results.append(word)
+        # return sorted(results, key=len)[-1]
+
+        # keep track of longest substring
+        longest = 0
+        for index in range(len(s)):
+            # use set for unique chars
+            seen = set()
+            for letter in s[index:]:
+                if letter in seen:
+                    break
+                # add if not in seen
+                seen.add(letter)
+            # return 
+            longest = max(longest, len(seen))
+        return longest
+
+class Test(unittest.TestCase):
+
+    def test_longest_substring_1(self):
+        actual = length_of_longest_substring_no_repeats('abcabcbb')
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    def test_longest_substring_2(self):
+        actual = length_of_longest_substring_no_repeats('ilikeitttttl')
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_longest_substring_3(self):
+        actual = length_of_longest_substring_no_repeats('helloforever')
+        expected = 5
+        self.assertEqual(actual, expected)
+
+unittest.main(verbosity=2)
--- linked-list-problems/add-linked-lists/addll.py
+++ linked-list-problems/add-linked-lists/addll.py
@@ -75,34 +75,39 @@ def add_linked_lists(l1, l2):
     Returns: head node of linked list of sum in "reverse-digit" format.
     """
     out_head = out_tail = None
-    result = 0
+    carry = 0
 
+    # Loop over both lists stopping when *both* lists are done
     while l1 or l2:
-        
+
+        # If not done l1, get digit, move to next. If done, use 0.
         if l1:
             digit1 = l1.data
             l1 = l1.next
         else:
             digit1 = 0
-
+        # If not done l2, get digit, move to next. If done, use 0.
         if l2:
             digit2 = l2.data
             l2 = l2.next
         else:
             digit2 = 0 
 
-        new_digit = digit1 + digit2 + result
-        result, new_digit = divmod(new_digit, 10)
+        # Add together digits (along w/carry) & determine new carry    
+        new_digit = digit1 + digit2 + carry
+        carry, new_digit = divmod(new_digit, 10)
         
+        # Add to end of our out LL
         if not out_head:
             out_head = out_tail = Node(new_digit)
 
         else:
             out_tail.next = Node(new_digit)
             out_tail = out_tail.next
-    
-    if result:
-        out_tail.next = Node(result)
+            
+    # If we have any carry left over, add a new place for it
+    if carry:
+        out_tail.next = Node(carry)
 
     return out_head 
 
