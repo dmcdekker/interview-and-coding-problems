5f7dd417441c29b4edb4254ec4e3f1c992588733
dmcdekker <denisemdekker@gmail.com>
2018-10-08 13:02:13 -0700

    modify tests

--- graphs/breadth_first_search.py
+++ graphs/breadth_first_search.py
@@ -1,9 +1,8 @@
-graph = {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
+import unittest
+
+# Runtime = O(V + E)
+
+
 
 #    A
 #  /   \
@@ -16,6 +15,7 @@ graph = {'A': set(['B', 'C']),
 
 
 def bfs(graph, start):
+    '''Returns all nodes in graph'''
     visited, queue = set(), [start]
     while queue:
         vertex = queue.pop(0)
@@ -24,9 +24,10 @@ def bfs(graph, start):
             queue.extend(graph[vertex] - visited)
     return visited
 
-print(bfs(graph, 'A')) # {'B', 'C', 'A', 'F', 'D', 'E'}
+# print(bfs(graph, 'A')) # {'B', 'C', 'A', 'F', 'D', 'E'}
 
 def bfs_paths(graph, start, goal):
+    '''Returns all possible paths between starting node and goal node'''
     queue = [(start, [start])]
     while queue:
         (vertex, path) = queue.pop(0)
@@ -44,7 +45,41 @@ def shortest_path(graph, start, goal):
     except StopIteration:
         return None
 
-print(shortest_path(graph, 'A', 'F')) # ['A', 'C', 'F']
+# print(shortest_path(graph, 'A', 'F')) # ['A', 'C', 'F']
+
+class Test(unittest.TestCase):
 
 
+    def test_bfs(self):
+        actual = bfs({'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}, 'A')
+        expected = ({'B', 'C', 'A', 'F', 'D', 'E'})
+        self.assertEqual(actual, expected)
+
+    def test_bfs_paths(self):
+        input_graph = ({'A': set(['B', 'C']),
+                            'B': set(['A', 'D', 'E']),
+                            'C': set(['A', 'F']),
+                            'D': set(['B']),
+                            'E': set(['B', 'F']),
+                            'F': set(['C', 'E'])})
+        expected_result = ([['A', 'C', 'F'], ['A', 'B', 'E', 'F']])
+        result = list(bfs_paths(input_graph, 'A', 'F'))
+        self.assertEqual(expected_result, result)
+
+    def test_shortest_path(self):
+        actual = shortest_path({'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}, 'A', 'F')
+        expected = (['A', 'C', 'F'])
+        self.assertEqual(actual, expected)
+
+unittest.main(verbosity=2)
 
